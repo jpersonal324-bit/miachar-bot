16a4d7c05d0ae92dd82294388b943207a3c2d33a
jpersonal324-bit <jpersonal324@gmail.com>
2026-06-10 15:56:18 -0500

    Fix indentation in bot.py

--- bot.py
+++ bot.py
@@ -6,13 +6,7 @@ API_ID = int(os.environ["API_ID"])
 API_HASH = os.environ["API_HASH"]
 SESSION = os.environ["SESSION_STRING"]
 
-MENSAJE = (
-          "Hola bb\n"
-          "Videollamada 45 dolares 20 min\n"
-          "30 dolares 10 min\n"
-          "30 dolares 10 videos\n"
-          "Acepto PayPal Zelle y Remitly"
-)
+MENSAJE = "Hola bb\nVideollamada 45 dolares 20 min\n30 dolares 10 min\n30 dolares 10 videos\nAcepto PayPal Zelle y Remitly"
 
 respondidos = set()
 
@@ -20,11 +14,11 @@ client = TelegramClient(StringSession(SESSION), API_ID, API_HASH)
 
 @client.on(events.NewMessage(incoming=True, func=lambda e: e.is_private))
 async def handler(event):
-          sender_id = event.sender_id
-          if sender_id not in respondidos:
-                        respondidos.add(sender_id)
+    sender_id = event.sender_id
+        if sender_id not in respondidos:
+                respondidos.add(sender_id)
                         await event.respond(MENSAJE)
 
-      print("Userbot iniciado...")
+print("Userbot iniciado...")
 client.start()
 client.run_until_disconnected()
